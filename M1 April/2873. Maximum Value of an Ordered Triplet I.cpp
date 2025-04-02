73c0383c308a800c917a26526d5e41c22d71e40a
Sane Aalam <saneraza78692@gmail.com>
2025-04-03 03:22:04 +0530

    feat: 2 problems

---
+++ M1 April/2873. Maximum Value of an Ordered Triplet I.cpp
@@ -0,0 +1,18 @@
+
+// brurhforce-code 
+class Solution {
+    public long maximumTripletValue(int[] nums) {
+     long result = 0;
+        int n = nums.length;
+        for (int i=0;i<n-2;i++){
+            for (int j=i+1;j<n-1;j++){
+                for (int k=j+1;k<n;k++){
+                    if (i<j && j<k){
+                        result = Math.max(((long)nums[i]-(long)nums[j])*nums[k], result);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
